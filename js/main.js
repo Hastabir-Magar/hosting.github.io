704984057c64c85d540164a7634969555e9065e9
Hastabir Magar <61810845+Hastabir-Magar@users.noreply.github.com>
2023-11-20 17:27:07 +0545

    JavaScript Files all
    Signed-off-by: Hastabir Magar <61810845+Hastabir-Magar@users.noreply.github.com>

---
+++ js/main.js
@@ -0,0 +1,192 @@
+$(function() {
+
+  var siteSticky = function() {
+		$(".js-sticky-header").sticky({topSpacing:0});
+	};
+	siteSticky();
+
+	var siteMenuClone = function() {
+
+		$('.js-clone-nav').each(function() {
+			var $this = $(this);
+			$this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
+		});
+
+
+		setTimeout(function() {
+			
+			var counter = 0;
+      $('.site-mobile-menu .has-children').each(function(){
+        var $this = $(this);
+        
+        $this.prepend('<span class="arrow-collapse collapsed">');
+
+        $this.find('.arrow-collapse').attr({
+          'data-toggle' : 'collapse',
+          'data-target' : '#collapseItem' + counter,
+        });
+
+        $this.find('> ul').attr({
+          'class' : 'collapse',
+          'id' : 'collapseItem' + counter,
+        });
+
+        counter++;
+
+      });
+
+    }, 1000);
+
+		$('body').on('click', '.arrow-collapse', function(e) {
+      var $this = $(this);
+      if ( $this.closest('li').find('.collapse').hasClass('show') ) {
+        $this.removeClass('active');
+      } else {
+        $this.addClass('active');
+      }
+      e.preventDefault();  
+      
+    });
+
+		$(window).resize(function() {
+			var $this = $(this),
+				w = $this.width();
+
+			if ( w > 768 ) {
+				if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+			}
+		})
+
+		$('body').on('click', '.js-menu-toggle', function(e) {
+			var $this = $(this);
+			e.preventDefault();
+
+			if ( $('body').hasClass('offcanvas-menu') ) {
+				$('body').removeClass('offcanvas-menu');
+				$this.removeClass('active');
+			} else {
+				$('body').addClass('offcanvas-menu');
+				$this.addClass('active');
+			}
+		}) 
+
+		// click outisde offcanvas
+		$(document).mouseup(function(e) {
+	    var container = $(".site-mobile-menu");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('body').hasClass('offcanvas-menu') ) {
+					$('body').removeClass('offcanvas-menu');
+				}
+	    }
+		});
+	}; 
+	siteMenuClone();
+
+});
+
+
+(function($) {
+    // 'use strict';
+
+   
+ // Hero Slider
+ var mySwiper = new Swiper('.hero-slider', {
+	slidesPerView: 1,
+	spaceBetween: 0,
+	loop: true,
+});
+
+// Cause Slider
+var causesSlider = new Swiper('.causes-slider', {
+	slidesPerView: 3,
+	spaceBetween: 30,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	},
+	breakpoints: {
+		1200: {
+			slidesPerView: 2,
+			spaceBetween: 30,
+		},
+		768: {
+			slidesPerView: 1,
+			spaceBetween: 0,
+		}
+	}
+} );
+
+const swiper = new Swiper('.hero-slider', {
+	autoplay: {
+	  delay: 7000,
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		renderBullet: function (index, className) {
+			return '<span class="' + className + '">0' + (index + 1) + '</span>';
+		},
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev'
+	}
+  });
+ 
+})(jQuery);
+
+// ====Feature===
+(function ($) {
+    'use strict';
+
+    var imJs = {
+        m: function (e) {
+            imJs.d();
+            imJs.methods();
+        },
+        d: function (e) {
+            this._window = $(window),
+            this._document = $(document),
+            this._body = $('body'),
+            this._html = $('html')
+        },
+        methods: function (e) {
+            imJs.featherAtcivation();
+            imJs.smothScroll();
+            imJs.smothScroll_Two();           
+            imJs.wowActive();
+            imJs.awsActivation();            
+        },
+        
+        wowActive: function () {
+            new WOW().init();
+        },
+
+        smothScroll: function () {
+            $(document).on('click', '.smoth-animation', function (event) {
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: $($.attr(this, 'href')).offset().top - 50
+                }, 300);
+            });
+        },
+        // two scroll spy
+        smothScroll_Two: function () {
+            $(document).on('click', '.smoth-animation-two', function (event) {
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: $($.attr(this, 'href')).offset().top - 0
+                }, 300);
+            });
+        },
+        featherAtcivation: function () {
+            feather.replace()
+        },
+        awsActivation:function(e){
+            AOS.init();
+        },
+    }
+    imJs.m();
+})(jQuery, window);
